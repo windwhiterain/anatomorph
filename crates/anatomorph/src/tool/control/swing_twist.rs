ac0573e4a8efb4bcc2c265c352c9f755e759da9f
whitingyan <1712428442@qq.com>
2026-03-11 20:31:47 +0800

    .

--- crates/anatomorph/src/tool/control/swing_twist.rs
+++ crates/anatomorph/src/tool/control/swing_twist.rs
@@ -88,33 +88,49 @@ fn on_drag(
                 .global_transforms
                 .get(body_idx)?
                 .translation;
-            let direction = Unit::new_normalize(translation - camera_translation);
+            let look_direction = Unit::new_normalize(translation - camera_translation);
             let camera_rotation =
-                SO3::rotation_between_axis(&(camera_rotation * (-R3::z_axis())), &direction)?
+                SO3::rotation_between_axis(&(camera_rotation * (-R3::z_axis())), &look_direction)?
                     * camera_rotation;
-
-            let arc = event
-                .delta
-                .to_anatomorph()
-                .component_mul(&R2::new(1.0, -1.0))
-                .push(0.0);
-
-            let radius = R3::z();
-            let axis = Unit::new_normalize(radius.cross(&arc));
-            let angle = arc.norm() / 256.0;
-            let target_rotation =
-                camera_rotation * SO3::from_axis_angle(&axis, angle) * camera_rotation.conjugate();
-            let delta_rotation = if let Some(parent) = parent {
-                let parent_global_rotation = multibody_global_transforms
+            let (camera2joint, joint_look_direction) = if let Some(parent) = parent {
+                let world2joint = multibody_global_transforms
                     .global_transforms
                     .get(parent)?
-                    .rotation;
-                parent_global_rotation.conjugate() * target_rotation * parent_global_rotation
+                    .rotation
+                    .inverse();
+
+                (world2joint * camera_rotation, world2joint * look_direction)
             } else {
-                target_rotation
+                (camera_rotation, look_direction)
             };
+            let delta = event.delta.to_anatomorph();
+            let arc = Unit::new_normalize(
+                camera2joint * delta.component_mul(&R2::new(1.0, -1.0)).push(0.0),
+            );
             let joint = &mut multibody.swing_twist_joints[idx];
-            joint.class.swing = delta_rotation * joint.class.swing;
+            let radius = joint.class.swing;
+            let axis = Unit::new_normalize(radius.cross(&arc));
+            let angle = delta.norm() / 256.0;
+            let swing_rotation = SO3::from_axis_angle(&axis, angle);
+            let swing = &mut joint.class.swing;
+            let forward = swing.dot(&joint_look_direction) > 0.0;
+            *swing = swing_rotation * *swing;
+            let look_component = swing.dot(&joint_look_direction);
+            let safe_look_component = if forward {
+                look_component.max(0.1)
+            } else {
+                look_component.min(-0.1)
+            };
+            if safe_look_component != look_component {
+                let perp_component =
+                    swing.into_inner() - look_component * joint_look_direction.into_inner();
+                *swing = Unit::new_unchecked(
+                    safe_look_component * joint_look_direction.into_inner()
+                        + (1.0 - safe_look_component * safe_look_component).sqrt()
+                            / perp_component.norm()
+                            * perp_component,
+                )
+            }
         };
     }
 }
