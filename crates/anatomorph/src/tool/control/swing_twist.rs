66ef7b577904a399ce0b3f6d971d76248b89bf56
whitingyan <1712428442@qq.com>
2026-03-12 14:59:36 +0800

    .

--- crates/anatomorph/src/tool/control/swing_twist.rs
+++ crates/anatomorph/src/tool/control/swing_twist.rs
@@ -1,6 +1,5 @@
 use anatomorph_math::{
-    R2, R3, SE3, SO3,
-    bevy::{ToAnatomorph as _, ToBevy as _},
+    R1, R2, R3, SE3, SO3, bevy::{ToAnatomorph as _, ToBevy as _}
 };
 use bevy::{camera::visibility::RenderLayers, prelude::*};
 use nalgebra::Unit;
@@ -102,33 +101,25 @@ fn on_drag(
                 (camera_rotation, look_direction)
             };
             let delta = event.delta.to_anatomorph();
-            let arc = Unit::new_normalize(
-                camera2joint * delta.component_mul(&R2::new(1.0, -1.0)).push(0.0),
-            );
+            let delta_reject = 
+                camera2joint * delta.component_mul(&R2::new(1.0, -1.0)).push(0.0)/256.0;
             let joint = &mut multibody.joints.swing_twist[idx];
-            let radius = joint.class.swing;
-            let axis = Unit::new_normalize(radius.cross(&arc));
-            let angle = delta.norm() / 256.0;
-            let swing_rotation = SO3::from_axis_angle(&axis, angle);
-            let swing = &mut joint.class.swing;
-            let forward = swing.dot(&joint_look_direction) > 0.0;
-            *swing = swing_rotation * *swing;
-            let look_component = swing.dot(&joint_look_direction);
-            let safe_look_component = if forward {
-                look_component.max(0.1)
-            } else {
-                look_component.min(-0.1)
+            let swing = joint.class.swing;
+            let project_coef = swing.dot(&joint_look_direction);
+            let project = project_coef * joint_look_direction.into_inner();
+            let reject = swing.into_inner() - project + delta_reject;
+            let (reject,reject_norm_squared) = {
+                const SAFE_NORM:R1 = 0.9;
+                const SAFE_NORM_SQUARED:R1 = SAFE_NORM*SAFE_NORM;
+                let norm_squared = reject.norm_squared();
+                if norm_squared<SAFE_NORM_SQUARED{
+                    (reject,norm_squared)
+                }else{
+                    (reject/norm_squared.sqrt()*SAFE_NORM,SAFE_NORM_SQUARED)
+                }
             };
-            if safe_look_component != look_component {
-                let perp_component =
-                    swing.into_inner() - look_component * joint_look_direction.into_inner();
-                *swing = Unit::new_unchecked(
-                    safe_look_component * joint_look_direction.into_inner()
-                        + (1.0 - safe_look_component * safe_look_component).sqrt()
-                            / perp_component.norm()
-                            * perp_component,
-                )
-            }
+            let project = if project_coef > 0.0 {1.0}else{-1.0}*(1.0-reject_norm_squared).sqrt()*joint_look_direction.into_inner();
+            joint.class.swing = Unit::new_unchecked(project + reject);
         };
     }
 }
